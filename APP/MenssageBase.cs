0ffefc5bc96bcd43db92c4e45c4f84302d699501
gaabysaantaanaa-create <gaabysaantaanaa@gmail.com>
2025-11-05 23:04:48 +0000

    APP

--- APP/MenssageBase.cs
+++ APP/MenssageBase.cs
@@ -1,17 +1,11 @@
 public abstract class MessageBase
 {
-
     protected MessageBase(string message)
     {
-
         Message = message;
         SendAt = DateTime.Now;
-
     }
 
-    public strting Message { get; }
-
-    public DateTime SendAt { get; 
-    }
-
+    public string Message { get; }
+    public DateTime SendAt { get; }
 }
